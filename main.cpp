fbb2ee7e199ce270bd13a1d19968abefa0b8335e
NipunR <ramaninipun@gmail.com>
2019-03-06 00:19:04 +0530

    Removed everything unnecessary.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,5 @@
 
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <vector>
-#include "Imgui/imgui.h"
-#include "Imgui/imgui_impl_glfw.h"
-#include "Imgui/imgui_impl_opengl3.h"
-#include "Init.hpp"
-#include "Shaders.hpp"
 #include "Render.h"
-#include <glm/glm.hpp>
-static const GLfloat g_fragment_buffer_data[] =
-{
-	0.0f,0.0f,0.0f,
-	1.0f,-1.0f,0.0f,
-	0.0f,1.0f,0.0f,
-
-};
-GLuint VertexArrayID;
-void CheckGLError();
-void makeTriangle();
 int main()
 {		
 	Render render;
@@ -30,30 +10,3 @@ int main()
 	render.End();
 	return 0;
 }
-void SetImguiInputs()
-{
-
-}
-
-void CheckGLError()
-{
-	GLenum err = glGetError();
-	if (err != GL_NO_ERROR)
-	{
-		std::string error;
-
-		switch (err) {
-		case GL_INVALID_OPERATION:      error = "INVALID_OPERATION";      break;
-		case GL_INVALID_ENUM:           error = "INVALID_ENUM";           break;
-		case GL_INVALID_VALUE:          error = "INVALID_VALUE";          break;
-		case GL_OUT_OF_MEMORY:          error = "OUT_OF_MEMORY";          break;
-		case GL_INVALID_FRAMEBUFFER_OPERATION:  error = "INVALID_FRAMEBUFFER_OPERATION";  break;
-		}
-
-		Init::LogError(error);
-		err = glGetError();
-	}
-}
-
-
-
