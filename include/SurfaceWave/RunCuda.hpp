3294f2f976d1215b07ced7cb94e372bff9d7271b
Moth <ramaninipun@gmail.com>
2019-12-02 20:48:16 +0530

    changed cuda version

--- include/SurfaceWave/RunCuda.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "../../Extra/helper_functions.h"
-#include "../../Extra/helper_cuda.h"
-#include <cuda_runtime.h>
-#include <cuda_gl_interop.h>
-void UpdateMesh_(float3 *pos, unsigned int width, unsigned int height, float time);
